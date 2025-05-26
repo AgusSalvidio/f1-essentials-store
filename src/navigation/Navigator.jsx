42fbcb5f67f663df0c8aa488b6c455a5c4f8e871
Agustin Salvidio <agustinsalvidio@gmail.com>
2025-05-26 20:53:48 -0300

    :sparkles: Add tab navigator

--- src/navigation/Navigator.jsx
+++ src/navigation/Navigator.jsx
@@ -1,19 +1,10 @@
-import Home from "../../src/screens/Home/Home";
-import ItemListCategory from "../../src/screens/ItemListCategory/ItemListCategory";
-import ItemDetail from "../../src/screens/ItemDetail/ItemDetail";
-
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
+import BottomTabNavigator from "./BottomTabNavigator/BottomTabNavigator";
 
-const Stack = createNativeStackNavigator();
 const Navigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen component={Home} name="Home" />
-        <Stack.Screen component={ItemListCategory} name="ItemListCategory" />
-        <Stack.Screen component={ItemDetail} name="ItemDetail" />
-      </Stack.Navigator>
+      <BottomTabNavigator />
     </NavigationContainer>
   );
 };

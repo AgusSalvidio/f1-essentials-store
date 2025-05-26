15ba92b9744aba8c13445ecd13cab466acdf8b30
Agustin Salvidio <agustinsalvidio@gmail.com>
2025-05-26 16:16:29 -0300

    :lipstick: Add Home view

---
+++ f1-essentials-store/src/screens/Home/Home.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { FlatList, View } from "react-native";
+import categories from "../../data/categories.json";
+import Categories from "../../components/Categories/Categories";
+import { styles } from "./Home.styles";
+
+const Home = ({ setCategorySelected }) => {
+  return (
+    <View style={styles.flatListContainer}>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        data={categories.sort()}
+        renderItem={({ item }) => (
+          <Categories category={item} selectCategory={setCategorySelected} />
+        )}
+        keyExtractor={(itemElement) => itemElement}
+        numColumns={2}
+        columnWrapperStyle={styles.row}
+        contentContainerStyle={styles.listContent}
+      />
+    </View>
+  );
+};
+
+export default Home;

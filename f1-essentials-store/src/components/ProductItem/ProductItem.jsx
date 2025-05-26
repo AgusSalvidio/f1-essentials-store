1eb3559bee0001c42b7a44dc08faf2079d10cdce
Agustin Salvidio <agustinsalvidio@gmail.com>
2025-05-26 16:19:17 -0300

    :lipstick: Add product item component

---
+++ f1-essentials-store/src/components/ProductItem/ProductItem.jsx
@@ -0,0 +1,24 @@
+import { Image, Pressable, Text } from "react-native";
+import React from "react";
+import Card from "../Card/Card";
+import { styles } from "./ProductItem.styles";
+
+const ProductItem = ({ product, setItemIdSelected = () => {} }) => {
+  return (
+    <Card style={styles.additionalStylesCard}>
+      <Pressable
+        style={styles.pressable}
+        onPress={() => setItemIdSelected(product.id)}
+      >
+        <Text style={styles.textCategory}>{product.title}</Text>
+        <Image
+          resizeMode="cover"
+          style={styles.image}
+          source={{ uri: product.images[0] }}
+        />
+      </Pressable>
+    </Card>
+  );
+};
+
+export default ProductItem;

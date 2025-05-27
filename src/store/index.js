b460f8eee5ceea73fb4cb48d5503cdd127514222
Agustin Salvidio <agustinsalvidio@gmail.com>
2025-05-27 08:58:19 -0300

    :sparkles: Add store provider

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import counterReducer from "../features/Counter/counterSlice";
+import shopReducer from "../features/Shop/shopSlice";
+
+export default configureStore({
+  reducer: {
+    counter: counterReducer,
+    shop: shopReducer,
+  },
+});

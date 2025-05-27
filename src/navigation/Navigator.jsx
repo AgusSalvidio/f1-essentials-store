44da23dbc0730482a5bf281be8a49223d2c71b55
Agustin Salvidio <agustinsalvidio@gmail.com>
2025-05-27 14:52:16 -0300

    :passport_control: Add auth

--- src/navigation/Navigator.jsx
+++ src/navigation/Navigator.jsx
@@ -1,10 +1,13 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { useSelector } from "react-redux";
 import BottomTabNavigator from "./BottomTabNavigator/BottomTabNavigator";
+import AuthStackNavigator from "./AuthStackNavigator/AuthStackNavigator";
 
 const Navigator = () => {
+  const { user } = useSelector((state) => state.auth.value);
   return (
     <NavigationContainer>
-      <BottomTabNavigator />
+      {user ? <BottomTabNavigator /> : <AuthStackNavigator />}
     </NavigationContainer>
   );
 };
